8fe2c5a1f01b4b700e206c1c8e944bc6c46e9287
mmm-nana <48428284+mmm-nana@users.noreply.github.com>
2023-01-17 17:31:31 +0900

    Update app.js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -52,6 +52,7 @@ function TodoListCard() {
     return (
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
+            <h1>ToDo App by Nanako Michiura</h1>
             {items.length === 0 && (
                 <p className="text-center">No items yet! Add one above!</p>
             )}
